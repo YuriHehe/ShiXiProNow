93cbfccab89d2b12b55f94d7041d8560ada1d8e2
YuriHehe <455080205@qq.com>
2018-06-30 11:25:04 +0800

    权限拦截完成了;)

--- src/main/java/com/pro/ssm/controller/AppController.java
+++ src/main/java/com/pro/ssm/controller/AppController.java
@@ -28,38 +28,26 @@ public class AppController {
 
     @ResponseBody
     @RequestMapping(value = "/testChangePsd",method = RequestMethod.GET)
-    public Map<String,Object> testChangePsd(HttpServletRequest request, Model model){
-        Map<String,Object> res;
-        if(request.getSession().getAttribute("login").equals("1")){
-            String userid = request.getSession().getAttribute("userid").toString();
-            String role = request.getSession().getAttribute("role").toString();
-            String oldpsd = request.getParameter("old");
-            String newpsd = request.getParameter("new");
-            res = userService.change_password(userid,role,oldpsd,newpsd);
-        }
-        else{
-            res = new HashMap<String, Object>();
-            res.put("code", Msg.CodeType.UNLOGIN);
-            res.put("msg", "未登录");
-            res.put("data", null);
-        }
-        return res;
+    public Map<String,Object> testChangePsd(HttpServletRequest request, Model model) {
+        String userid = request.getSession().getAttribute("userid").toString();
+        String role = request.getSession().getAttribute("role").toString();
+        String oldpsd = request.getParameter("old");
+        String newpsd = request.getParameter("new");
+        return userService.change_password(userid, role, oldpsd, newpsd);
     }
 
     @ResponseBody
     @RequestMapping(value = "/testLogin",method = RequestMethod.GET)
-    public Map<String,Object> testLogin(HttpServletRequest request, @RequestParam("userid")String userid, @RequestParam("role")String role){
-        Map<String,Object> res = new HashMap<String, Object>();
-        request.getSession().setAttribute("login","1");
-        request.getSession().setAttribute("userid",userid);
-        request.getSession().setAttribute("role",role);
+    public Map<String,Object> testLogin(HttpServletRequest request, @RequestParam("userid")String userid, @RequestParam("role")String role, @RequestParam("password")String password){
+        //检查是否存在于数据库
+        Map<String,Object> res = userService.userCheck(userid,password,role);
+        if(res.get("code").equals(200)){
+            request.getSession().setAttribute("login","1");
+            request.getSession().setAttribute("userid",userid);
+            request.getSession().setAttribute("role",role);
+            return Msg.Success("登陆成功");
+        }
         return res;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/loginError", method = RequestMethod.GET)
-    public Map<String,Object> loginError(HttpServletResponse response, HttpServletRequest request){
-        return Msg.Error("权限错误");
     }
 
     @ResponseBody
@@ -70,10 +58,11 @@ public class AppController {
         String role = request.getParameter("role");
         //检查是否存在于数据库
         Map<String,Object> res = userService.userCheck(userid,password,role);
-        if(res.get("code").equals(Msg.CodeType.SUCCESS)){
+        if(res.get("code").equals(200)){
             request.getSession().setAttribute("login","1");
             request.getSession().setAttribute("userid",userid);
             request.getSession().setAttribute("role",role);
+            return Msg.Success("登陆成功");
         }
         return res;
     }
@@ -84,46 +73,26 @@ public class AppController {
         request.getSession().removeAttribute("login");
         request.getSession().removeAttribute("userid");
         request.getSession().removeAttribute("role");
-        Map<String, Object> res = new HashMap<String, Object>();
-        res.put("code", Msg.CodeType.UNLOGIN);
-        res.put("msg", "未登录");
-        res.put("data", null);
-        return res;
+        return Msg.Success("成功注销");
     }
 
     @ResponseBody
     @RequestMapping(value = "/change_password", method = RequestMethod.POST)
-    public Map<String,Object> changePassword(HttpServletRequest request, Model model){
-        Map<String,Object> res;
-        if(request.getSession().getAttribute("login").equals("1")){
-            String userid = request.getSession().getAttribute("userid").toString();
-            String role = request.getSession().getAttribute("role").toString();
-            String oldpsd = request.getParameter("old");
-            String newpsd = request.getParameter("new");
-            res = userService.change_password(userid,role,oldpsd,newpsd);
-        }
-        else{
-            res = new HashMap<String, Object>();
-            res.put("code", Msg.CodeType.UNLOGIN);
-            res.put("msg", "未登录");
-            res.put("data", null);
-        }
-        return res;
+    public Map<String,Object> changePassword(HttpServletRequest request, Model model) {
+        String userid = request.getSession().getAttribute("userid").toString();
+        String role = request.getSession().getAttribute("role").toString();
+        String oldpsd = request.getParameter("old");
+        String newpsd = request.getParameter("new");
+        return userService.change_password(userid, role, oldpsd, newpsd);
     }
 
     @ResponseBody
     @RequestMapping(value = "/showStatue",method = RequestMethod.GET)
-    public Map<String,Object> showStatue(HttpServletRequest request, Model model){
-        Map<String,Object> res = new HashMap<String, Object>();
-        if(request.getSession().getAttribute("login") == null){
-            res.put("userid", "null");
-            res.put("msg", "未登录");
-        }
-        else{
-            res.put("userid", (String)request.getSession().getAttribute("userid"));
-            res.put("login", (String)request.getSession().getAttribute("login"));
-            res.put("role", (String)request.getSession().getAttribute("role"));
-        }
-        return res;
+    public Map<String,Object> showStatue(HttpServletRequest request, Model model) {
+        Map<String, Object> res = new HashMap<String, Object>();
+        res.put("userid", (String) request.getSession().getAttribute("userid"));
+        res.put("login", (String) request.getSession().getAttribute("login"));
+        res.put("role", (String) request.getSession().getAttribute("role"));
+        return Msg.Success("成功", res);
     }
 }
